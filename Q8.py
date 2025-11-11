26c0b19aafcfc2fdc0215e0a70bfc6098fbc58ab
Surya Pratap <suryapratap2154@gmail.com>
2025-11-11 16:45:01 +0530

    Create Q8.py

---
+++ Q8.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+
+print(f"NumPy version: {np.__version__}")
+print(f"Pandas version: {pd.__version__}")
+print(f"Matplotlib version: {mpl.__version__}")

91ed4cd0dba41f36457f83b5e5cdd532b4810e97
Surya Pratap <suryapratap2154@gmail.com>
2025-11-11 16:42:49 +0530

    Create Q2.py

---
+++ Q2.py
@@ -0,0 +1,7 @@
+# Using venv
+python -m venv data_science_env
+source data_science_env/bin/activate  # On Windows: data_science_env\Scripts\activate
+
+# Or using conda
+conda create -n data_science_env python=3.9
+conda activate data_science_env
